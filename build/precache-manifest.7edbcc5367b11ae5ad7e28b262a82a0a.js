d8749399f8971aba6b698b5bbc689cd72c86f532
seif1000 <seifwin74@gmail.com>
2020-02-07 16:55:16 +0100

    build

---
+++ build/precache-manifest.7edbcc5367b11ae5ad7e28b262a82a0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f467a23ae9c10744b06680d15659f3e4",
+    "url": "/React-dark-sky-clone/index.html"
+  },
+  {
+    "revision": "04871b194395aaadbca6",
+    "url": "/React-dark-sky-clone/static/css/main.83492628.chunk.css"
+  },
+  {
+    "revision": "f49e378a11e3bdff964a",
+    "url": "/React-dark-sky-clone/static/js/2.4b69e0d5.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/React-dark-sky-clone/static/js/2.4b69e0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04871b194395aaadbca6",
+    "url": "/React-dark-sky-clone/static/js/main.f9c4fbf0.chunk.js"
+  },
+  {
+    "revision": "7ddd256b2e93d6bde8f6",
+    "url": "/React-dark-sky-clone/static/js/runtime-main.c53d9253.js"
+  },
+  {
+    "revision": "cf6f66afcb21ef901123c095ba5b8f7d",
+    "url": "/React-dark-sky-clone/static/media/logo.cf6f66af.png"
+  },
+  {
+    "revision": "c00ac9943a86ac0c62f8a16f9bca7ed6",
+    "url": "/React-dark-sky-clone/static/media/rain.c00ac994.png"
+  },
+  {
+    "revision": "ef1890cd01f2c2a596b30cf614fa0e85",
+    "url": "/React-dark-sky-clone/static/media/snowy.ef1890cd.png"
+  }
+]);
